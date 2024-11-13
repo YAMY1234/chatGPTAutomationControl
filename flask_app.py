07d6af161fb48cf484c5457f273e3df49792fd33
YAMY <2682000734@qq.com>
2024-11-13 10:28:41 -0800

    update cors

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify
-import pyautogui
-import pyperclip
-import time
+from flask_cors import CORS
 from automation_script import automate_text_interaction
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/call_chatgpt', methods=['POST'])
 def call_chatgpt():
